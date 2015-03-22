c4c378159c1a9c9780bcf366e572bec9fbd1a843
Andrew Clarkson <andrew@clarkson.mn>
2015-03-22 12:41:26 -0500

    Final commit of Red Hot Hack 2015

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from hashlib import sha1
 
 from flask import Flask, request, url_for, render_template, redirect, send_from_directory, jsonify
 from flask_wtf import Form
+from wtforms.validators import DataRequired
+from wtforms.fields import IntegerField
 from flask_wtf.file import FileField, FileAllowed, FileRequired
 from flask.ext.sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import IntegrityError
@@ -14,6 +16,61 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 app.config['SECRET_KEY'] = 'Super Secret'
 db = SQLAlchemy(app)
 
+import os
+import sys
+
+def spawn_daemon(path_to_executable, *args):
+    """Spawn a completely detached subprocess (i.e., a daemon).
+
+    E.g. for mark:
+    spawnDaemon("../bin/producenotify.py", "producenotify.py", "xx")
+    """
+    # fork the first time (to make a non-session-leader child process)
+    try:
+        pid = os.fork()
+    except OSError:
+        raise RuntimeError("1st fork failed: %s [%d]")
+    if pid != 0:
+        # parent (calling) process is all done
+        return
+
+    # detach from controlling terminal (to make child a session-leader)
+    os.setsid()
+    try:
+        pid = os.fork()
+    except OSError:
+        raise RuntimeError("2nd fork failed: %s [%d]")
+    if pid != 0:
+        # child process is all done
+        os._exit(0)
+
+    # grandchild process now non-session-leader, detached from parent
+    # grandchild process must now close all open files
+    try:
+        maxfd = os.sysconf("SC_OPEN_MAX")
+    except (AttributeError, ValueError):
+        maxfd = 1024
+
+    for fd in range(maxfd):
+        try:
+           os.close(fd)
+        except OSError: # ERROR, fd wasn't open to begin with (ignored)
+           pass
+
+    # redirect stdin, stdout and stderr to /dev/null
+    os.open(os.devnull, os.O_RDWR)  # standard input (0)
+    os.dup2(0, 1)
+    os.dup2(0, 2)
+
+    # and finally let's execute the executable for the daemon!
+    try:
+        os.execv(path_to_executable, args)
+    except Exception:
+        # oops, we're cut off from the world, let's just give up
+        os._exit(255)
+
+
+
 class UploadForm(Form):
     """
     Having a upload form
@@ -21,6 +78,9 @@ class UploadForm(Form):
     photo = FileField(u'Photo', validators=[
         FileRequired(), 
         FileAllowed(['jpg'], 'images only')
+    ])
+    age = IntegerField(u'Age', validators=[
+        DataRequired()    
     ])
 
 
@@ -72,6 +132,7 @@ def index():
         
         mole = Mole()
         mole.id = hash
+        mole.age = form.age.data
         mole.filename = filename
 
         db.session.add(mole)
@@ -105,7 +166,7 @@ def refine(id):
     db.session.merge(mole)
     db.session.commit()
 
-    os.spawnlp(os.P_NOWAIT, 'algorithm.py', 'algorithm.py', id)
+    spawn_daemon(path.join(os.getcwd(), 'algorithm.py'), 'algorithm.py', id)
 
     return jsonify({'OK': 'True'}) 
 
